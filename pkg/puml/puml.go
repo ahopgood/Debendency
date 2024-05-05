d0e94e11a8a4d5ef3b062937b33d26175bf2f46a
Alexander Hopgood <alex.hopgood@gmail.com>
2024-05-05 13:46:39 +0100

    Prototype puml diagram

---
+++ pkg/puml/puml.go
@@ -0,0 +1,80 @@
+package puml
+
+import (
+	"com/alexander/debendency/pkg"
+	"fmt"
+)
+
+func GenerateDiagram(modelMap map[string]*pkg.PackageModel) Uml {
+	dependencies := make([]Dependency, 0)
+	// Loop through the model dependencies?
+	for key, fromModel := range modelMap {
+		fmt.Printf("%s %#v\n", key, fromModel)
+		for _, toModel := range fromModel.Dependencies {
+			fmt.Printf("From %s to %s\n", fromModel.Name, toModel.Name)
+			dependencies = append(dependencies, Dependency{
+				From: fromModel.Name,
+				To:   toModel.Name,
+			})
+		}
+	}
+	puml := NewUml(
+		NewDigraph(dependencies),
+	)
+	return puml
+}
+
+type Uml struct {
+	start   string
+	diagram UmlDiagram
+	end     string
+}
+
+func NewUml(umlDiagram UmlDiagram) Uml {
+	return Uml{
+		start:   "@startuml\n",
+		diagram: umlDiagram,
+		end:     "@enduml",
+	}
+}
+
+func (uml Uml) Contents() string {
+	return uml.start + uml.diagram.Contents() + uml.end
+}
+
+type UmlDiagram interface {
+	Contents() string
+}
+
+type Digraph struct {
+	start        string
+	dependencies []Dependency
+	end          string
+}
+
+func NewDigraph(dependencies []Dependency) Digraph {
+	return Digraph{
+		start:        "digraph test {\n",
+		dependencies: dependencies,
+		end:          "}\n",
+	}
+}
+
+// Represents a dependency From one thing To another
+// E.g. "salt-master" -> "salt-common"
+type Dependency struct {
+	From string
+	To   string
+}
+
+// Uml Diagram implementation
+func (d Digraph) Contents() string {
+	fmt.Println("Building diagraph contents")
+	output := d.start + "\n"
+	for _, value := range d.dependencies {
+		output = output + "\t" + "\"" + value.From + "\"" + " -> " + "\"" + value.To + "\"" + "\n"
+
+	}
+	output = output + "\n" + d.end + "\n"
+	return output
+}
